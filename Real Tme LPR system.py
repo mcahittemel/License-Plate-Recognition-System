3eea6328e4500ac885020287cb544ab65dcf3adc
Muhammet Cahit TEMEL <85481762+mcahittemel@users.noreply.github.com>
2021-06-07 11:28:14 +0300

    Add files via upload

---
+++ Real Tme LPR system.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import pytesseract
+import imutils
+import RPi.GPIO as GPIO
+import timeit
+import time
+
+cap = cv2.VideoCapture(0)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+while True:
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    filtered = cv2.bilateralFilter(gray, 5, 75,75)
+    histogram_e = cv2.equalizeHist(filtered)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    morphology = cv2.morphologyEx(histogram_e, cv2.MORPH_OPEN, kernel,iterations=15)
+    gcikarilmisresim = cv2.subtract(histogram_e,morphology)
+    edged = cv2.Canny(gcikarilmisresim, 30, 250)
+
+    contours = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(contours)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
+    screen = None
+
+    for c in cnts:
+        epsilon = 0.018 * cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, epsilon, True)
+        if len(approx) == 4:
+            screen = approx
+            break
+
+    mask = np.zeros(gray.shape, np.uint8)
+    new_cap = cv2.drawContours(mask, [approx], 0, (255, 255, 255), -1)
+    new_cap = cv2.bitwise_and(frame,frame,mask=mask)
+
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
+
+    ret, binary = cv2.threshold(cropped, 110, 255,cv2.THRESH_BINARY)
+
+
+    kernel = np.ones((3, 3), np.uint8)
+    binaryerosion = cv2.erode(binary, kernel, iterations=1)
+    binaryopening = cv2.dilate(binaryerosion, kernel, iterations=1)
+
+    cv2.imshow("Original License Plate", frame)
+    # cv2.imshow("Gray License Plate",gray)
+    # cv2.imshow("Filtered License Plate",filtered)
+    # cv2.imshow("histogram eşikleme",histogram_e)
+    # cv2.imshow("Morphology",morphology)
+    # cv2.imshow("goruntuden cikarilmis resim",gcikarilmisresim)
+    #cv2.imshow("Egded License Plate", edged)
+    # cv2.imshow("Mask License Plate",mask)
+    # cv2.imshow("New License Plate",new_img)
+    cv2.imshow("Cropped License Plate", cropped)
+    # cv2.imshow("binary",binaryerosion)
+    # cv2.imshow("binaryopening",binaryopening)
+
+    custom_config = r' -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 11'
+    LP = pytesseract.image_to_string(cropped, config=custom_config)
+    print("detecting the license plate: ", LP[:-2])
+
+
+
+    if cv2.waitKey(500) & 0xFF == ord("q"):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
